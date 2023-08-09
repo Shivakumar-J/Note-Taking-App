c625a26576cdd03903cadda47c883bac037063dd
Siva Kumar Jadalaiah Gari <100525166+Shivakumar-J@users.noreply.github.com>
2023-08-09 16:07:18 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('add-note-form');
   const notesList = document.getElementById('notes');
+  const editorContainer = document.getElementById('editor-container');
+  
+  const quill = new Quill('#editor-container', {
+  theme: 'snow'
+});
+
+  let editedNoteId = null; // To store the ID of the note being edited
 
   // Function to render notes
   function renderNotes() {
@@ -14,14 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
           li.innerHTML = `
             <h3>${note.title}</h3>
             <p>${note.content}</p>
-            <button class="edit-btn">Edit</button>
-            <button class="delete-btn">Delete</button>
+            <button class="edit-btn">Edit 🖊️</button>
+            <button class="delete-btn">Delete 🚮</button>
           `;
           const editBtn = li.querySelector('.edit-btn');
           const deleteBtn = li.querySelector('.delete-btn');
 
           // Attach event listeners to edit and delete buttons
-          editBtn.addEventListener('click', () => editNote(note.id));
+          editBtn.addEventListener('click', () => editNote(note));
           deleteBtn.addEventListener('click', () => deleteNote(note.id));
 
           notesList.appendChild(li);
@@ -32,10 +39,45 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   }
 
+  // Function to edit a note
+  function editNote(note) {
+    editedNoteId = note.id;
+
+    // Fill the form with the note's title and content
+    document.getElementById('title').value = note.title;
+    quill.root.innerHTML = note.content;
+  }
+
+  // Function to update an edited note
+  function updateNote() {
+    const title = document.getElementById('title').value;
+    const content = quill.root.innerHTML; // Retrieve content from Quill editor
+  
+    fetch(`/notes/${editedNoteId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ title, content })
+    })
+      .then(response => response.json())
+      .then(note => {
+        form.reset();
+        quill.root.innerHTML = ''; // Clear Quill editor content
+        renderNotes();
+        editedNoteId = null;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
+  
+
   // Function to add a new note
   function addNote() {
     const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
+    const content = quill.root.innerHTML;
 
     fetch('/notes', {
       method: 'POST',
@@ -48,28 +90,6 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(note => {
         console.log('Note added:', note);
         form.reset();
-        renderNotes();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-
-  // Function to edit a note
-  function editNote(id) {
-    const title = prompt('Enter new title:');
-    const content = prompt('Enter new content:');
-
-    fetch(`/notes/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title, content })
-    })
-      .then(response => response.json())
-      .then(note => {
-        console.log('Note edited:', note);
         renderNotes();
       })
       .catch(error => {
@@ -93,8 +113,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   form.addEventListener('submit', event => {
     event.preventDefault();
-    addNote();
+    if (editedNoteId !== null) {
+      updateNote();
+    } else {
+      addNote();
+    }
   });
 
   renderNotes();
 });
+
